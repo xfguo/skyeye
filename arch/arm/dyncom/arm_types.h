1a536a5a495d17e34a154984a3facf369a6d7820
Michael.Kang <blackfin.kang@gmail.com>
2010-11-16 20:13:32 +0800

    Add dyncom decoder for arm platform.
    Add dyncom decoder for arm platform.

---
+++ arch/arm/dyncom/arm_types.h
@@ -0,0 +1,39 @@
+/*
+ * libcpu: arm_types.h
+ *
+ * the register file
+ */
+
+typedef struct {
+	union {
+		struct {
+			uint32_t r0;
+			uint32_t r1;
+			uint32_t r2;
+			uint32_t r3;
+			uint32_t r4;
+			uint32_t r5;
+			uint32_t r6;
+			uint32_t r7;
+			uint32_t r8;
+			uint32_t r9;
+			uint32_t r10;
+			uint32_t r11;
+			uint32_t r12;
+			uint32_t r13;
+			uint32_t r14;
+			uint32_t r15; /* would be PC, but we store PC externally, so this is unused */
+			uint32_t cpsr;
+		};
+		uint32_t r[17];
+	};
+	uint32_t pc;
+} reg_arm_t;
+
+typedef struct {
+	Value* ptr_N;
+	Value* ptr_Z;
+	Value* ptr_C;
+	Value* ptr_V;
+	Value* ptr_I;
+} ccarm_t;
