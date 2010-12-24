b7e75016d254e8726cba604897a4d9a64ba0754c
Jeff Du <jeffdo.du@gmail.com>
2010-12-24 21:15:11 +0800

    change arm_core_init global for arm dyncom
    change arm_core_init global for arm dyncom
    
    Signed-off-by: Jeff Du <jeffdo.du@gmail.com>

--- arch/arm/common/arm_arch_interface.c
+++ arch/arm/common/arm_arch_interface.c
@@ -18,7 +18,7 @@ int big_endian = 0;
 static cpu_config_t *p_arm_cpu;
 static ARMword preset_regfile[16];
 extern ARMword skyeye_cachetype;
-static void arm_core_init (ARMul_State *state, int i);
+void arm_core_init (ARMul_State *state, int i);
 
 static void arm_step_once ();
 //chy 2005-08-01, borrow from wlm's 2005-07-26's change
@@ -117,7 +117,7 @@ arm_init_state ()
 	arm_cpu_init();
 }
 
-static void
+void
 arm_core_init (ARMul_State *state, int i)
 {
 	ARMul_NewState (state);
