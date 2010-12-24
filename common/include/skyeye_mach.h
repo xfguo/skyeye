69e07dba48cccb2ba7133206369ccfddc9206d52
Michael.Kang <blackfin.kang@gmail.com>
2010-12-24 16:41:47 +0800

    Add some copyright declaration
    Add some copyright declaration

--- common/include/skyeye_mach.h
+++ common/include/skyeye_mach.h
@@ -1,3 +1,24 @@
+/*
+ *	skyeye_mach.h - The mach related definition  
+ *
+ *	Copyright (C) 2010 Michael.kang (blackfin.kang@gmail.com)
+ *
+ *	This program is free software; you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License version 2 as
+ *	published by the Free Software Foundation.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program; if not, write to the Free Software
+ *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+/*
+ * 05/16/2009   Michael.Kang  <blackfin.kang@gmail.com>
+ */
 #ifndef __SKYEYE_MACH_H__
 #define __SKYEYE_MACH_H__
 #include "skyeye_types.h"
