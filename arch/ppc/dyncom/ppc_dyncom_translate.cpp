04f23cceb107f2641c088fcc6da3fe588d6fd82f
Michael.Kang <blackfin.kang@gmail.com>
2011-01-19 17:00:42 +0800

    Ignore some null tag function for the unreachable
    Ignore some null tag function for the unreachable code flow.
    
    Signed-off-by: Michael.Kang <blackfin.kang@gmail.com>

--- arch/ppc/dyncom/ppc_dyncom_translate.cpp
+++ arch/ppc/dyncom/ppc_dyncom_translate.cpp
@@ -50,7 +50,13 @@ int arch_powerpc_tag_instr(cpu_t *cpu, addr_t phys_pc, tag_t *tag, addr_t *new_p
 	if(!opc_func)
 		BAD_INSTR;
 	assert(opc_func->tag != NULL);
-	opc_func->tag(cpu, instr, phys_pc, tag, new_pc, next_pc);
+	if(opc_func->tag == NULL){
+		printf("Some NULL tag functions at 0x%x\n", phys_pc);	
+		opc_default_tag(cpu, instr, phys_pc,tag, new_pc, next_pc);
+	}
+	else
+		opc_func->tag(cpu, instr, phys_pc, tag, new_pc, next_pc);
+
 	*next_pc = phys_pc + instr_size;
 	if(phys_pc == END_ADDR)
 		*tag |= TAG_STOP;
