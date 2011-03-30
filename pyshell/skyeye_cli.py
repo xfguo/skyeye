a3663145fcc496eb1fc30a83f40d7d4d70ab8b33
yuanzheng <tsinghuayuan86@gmail.com>
2011-03-30 15:58:29 +0800

    Add the help of every command through help+cmd.
    Add the help of every command through help+cmd.
    
    Signed-off-by: yuanzheng <tsinghuayuan86@gmail.com>
    Signed-off-by: Michael.Kang <blackfin.kang@gmail.com>

--- pyshell/skyeye_cli.py
+++ pyshell/skyeye_cli.py
@@ -2,6 +2,12 @@ import cmd, sys
 import skyeye_lib_path
 from skyeye_module import *
 
+def help_info_open():
+    print "help info:"
+    print "==========================================================="
+
+def help_info_close():
+    print "==========================================================="
 
 class SkyEyeCli(cmd.Cmd):
 
@@ -136,20 +142,41 @@ class SkyEyeCli(cmd.Cmd):
 
 ####list-module list-options, list-machines, list-bp ####
     def do_list_modules(self, arg):
-        print "In do_list_modules"
+#        print "In do_list_modules"
         libcommon.com_list_modules(arg)
 
     def do_list_options(self, arg):
-        print "In do_list_options"
+#        print "In do_list_options"
         libcommon.com_list_options(arg)
 
     def do_list_machines(self, arg):
-        print "In do_list_machines"
+#        print "In do_list_machines"
         libcommon.com_list_machines(arg)
 
     def do_list_bp(self, arg):
-        print "In do_list_bp"
+#        print "In do_list_bp"
         libcommon.com_list_bp(arg)
+
+
+    def help_list_modules(self):
+        help_info_open()
+        print "list-modules : List all the loaded module."
+        help_info_close()
+
+    def help_list_options(self):
+        help_info_open()
+        print "list-options : List all the available options for SkyEye."
+        help_info_close()
+
+    def help_list_machines(self):
+        help_info_open()
+        print "list-machines : List all the supported machines for SkyEye."
+        help_info_close()
+
+    def help_list_bp(self):
+        help_info_open()
+        print "list-bp : List all the breakpoint."
+        help_info_close()
 
 ####list-module list-options, list-machines, list-bp ####
 
@@ -157,16 +184,33 @@ class SkyEyeCli(cmd.Cmd):
 ########## show-pref, show-map, show-step ###############
 
     def do_show_pref(self, arg):
-        print "In do_show_pref"
+#        print "In do_show_pref"
         libcommon.com_show_pref(arg)
 
     def do_show_map(self, arg):
-        print "In do_show_map"
+#        print "In do_show_map"
         libcommon.com_show_map(arg)
 
     def do_show_step(self, arg):
-        print "In do_show_step"
+#        print "In do_show_step"
         libcommon.com_show_step(arg)
+
+
+    def help_show_pref(self):
+        help_info_open()
+        print "show-pref : Show the current preference for SkyEye."
+        help_info_close()
+
+    def help_show_map(self):
+        help_info_open()
+        print "show-map : Show the current memory map for the machine."
+        help_info_close()
+
+    def help_show_step(self):
+        help_info_open()
+        print "show-step : Show the steps of current processor."
+        help_info_close()
+
 
 ########## show-pref, show-map, show-step ###############
 
@@ -174,105 +218,215 @@ class SkyEyeCli(cmd.Cmd):
 ############# cov-state, cov-off, cov-on ################
 
     def do_cov_state(self, arg):
-        print "In do_cov_state"
+#        print "In do_cov_state"
         libcodecov.cov_state_show(arg)
 
     def do_cov_off(self, arg):
-        print "In do_cov_off"
+#        print "In do_cov_off"
         libcodecov.cov_state_off(arg)
 
     def do_cov_on(self, arg):
-        print "In do_cov_on"
+#        print "In do_cov_on"
         libcodecov.cov_state_on(arg)
+
+    def help_cov_state(self):
+        help_info_open()
+        print "cov-state : show code coverage state."
+        help_info_close()
+
+    def help_cov_off(self):
+        help_info_open()
+        print "cov-off : turn off code coverage switch."
+        help_info_close()
+
+    def help_cov_on(self):
+        help_info_open()
+        print "cov-on : turn on code coverage switch."
+        help_info_close()
+
 
 ############# cov-state, cov-off, cov-on ################
 
 ################## load-conf ##########################
 
     def do_load_conf(self, arg):
-        print "In do_load_conf"
+#        print "In do_load_conf"
         libcommon.com_load_conf(arg)
+
+    def help_load_conf(self):
+        help_info_open()
+        print "load-conf : load a config file and parse it for SkyEye."
+        help_info_close()
 
 ################## load-conf ##########################
 
 ################### delete-bp ##########################
 
     def do_delete_bp(self, arg):
-        print "In do_delete_bp"
+#        print "In do_delete_bp"
         libcommon.com_delete_bp(arg)
+
+    def help_delete_bp(self):
+        help_info_open()
+        print "delete-bp : Delete some breakpoint."
+        help_info_close()
 
 ################### delete-bp ##########################
 
 #################### log-bus ##########################
 
     def do_log_bus(self, arg):
-        print "In do_log_bus"
+#        print "In do_log_bus"
         libbus_log.com_log_bus(arg)
+
+    def help_log_bus(self):
+        help_info_open()
+        print "log-bus : record every bus access to log file."
+        help_info_close()
 
 #################### log-bus ##########################
 
 #################### remote-gdb ########################
 
     def do_remote_gdb(self, arg):
-        print "In do_remote_gdb"
+#        print "In do_remote_gdb"
         libgdbserver.com_remote_gdb
 
+    def help_remote_gdb(self):
+        help_info_open()
+        print "remote-gdb : Open remote gdb debugger."
+        help_info_close()
 
 #################### remote-gdb ########################
 
 #################### other COMMANDS ####################
 
     def do_ls(self, arg):
-        print "outline ls"
+#        print "outline ls"
         libcommon.com_list(arg)
 
     def do_quit(self, arg):
-        print "outline quit"
+#        print "outline quit"
         libcommon.com_quit(arg)
 
+    do_q = do_quit
+
     def do_run(self, arg):
-        print "outline run"
+#        print "outline run"
         libcommon.com_run(arg)
 
     def do_stop(self, arg):
-        print "outline stop"
+#        print "outline stop"
         libcommon.com_stop(arg)
 
     def do_continue(self, arg):
-        print "outline continue"
+#        print "outline continue"
         libcommon.com_cont(arg)
 
     def do_stepi(self, arg):
-        print "outline stepi"
+#        print "outline stepi"
         libcommon.com_si(arg)
 
     def do_start(self, arg):
-        print "outline start"
+#        print "outline start"
         libcommon.com_start(arg)
 
     def do_info(self, arg):
-        print "outline info"
+#        print "outline info"
         libcommon.com_info(arg)
 
     def do_x(self, arg):
-        print "outline x"
+#        print "outline x"
         libcommon.com_x(arg)
 
     def do_pmon(self, arg):
-        print "outline pmon"
+#        print "outline pmon"
         libpmon.com_pmon(arg)
 
     def do_create_uart_16550(self, arg):
-        print "outline create_uart_16550"
+#        print "outline create_uart_16550"
         libuart_16550.com_create_16550
 
     def do_disassemble(self, arg):
-        print "outline disassemble"
+#        print "outline disassemble"
         libdisasm.com_disassemble(arg)
 
     def do_break(self, arg):
-        print "outline do_break"
+#        print "outline do_break"
         libcommon.com_break(arg)
+
+
+    def help_ls(self):
+        help_info_open()
+        print "ls : Synonym for `list'"
+        help_info_close()
+
+    def help_quit(self):
+        help_info_open()
+        print "quit : Quit SkyEye " 
+        help_info_close()
+
+    def help_q(self):
+        help_info_open()
+        print "q : Quit SkyEye " 
+        help_info_close()
+
+
+    def help_run(self):
+        help_info_open()
+        print "run : Start the simulator." 
+        help_info_close()
+
+    def help_stop(self):
+        help_info_open()
+        print "stop : Stop the running of simulator." 
+        help_info_close()
+
+    def help_continue(self):
+        help_info_open()
+        print "continue : Continue the running of interrupted simulator." 
+        help_info_close()
+
+    def help_stepi(self):
+        help_info_open()
+        print "stepi : step into ." 
+        help_info_close()
+
+    def help_start(self):
+        help_info_open()
+        print "start : start simulator." 
+        help_info_close()
+
+    def help_info(self):
+        help_info_open()
+        print "info : show information for various objects. "
+        help_info_close()
+
+    def help_x(self):
+        help_info_open()
+        print "x : display memory value at the address. "
+        help_info_close()
+
+    def help_pmon(self):
+        help_info_open()
+        print "pmon : enable the performance monitor." 
+        help_info_close()
+
+    def help_create_uart_16550(self):
+        help_info_open()
+        print "create_uart_16550 : Create a new uart of 16550 type."
+        help_info_close()
+
+    def help_disassemble(self):
+        help_info_open()
+        print "disassemble : Disassemble the given address."
+        help_info_close()
+
+    def help_break(self):
+        help_info_open()
+        print "break : disassemble : set breakpoint for an address."
+        help_info_close()
+
 
 #################### other COMMANDS ####################
 ##### End with Class SkyEye #####
