fa7cb55129dc83d6d86c24d60db2bb043467cc09
Michael.Kang <blackfin.kang@gmail.com>
2011-04-06 15:50:13 +0800

    Fix the bug for function log utility
    Fix the bug for function log utility

--- common/profile/bfd_target.c
+++ common/profile/bfd_target.c
@@ -5,10 +5,10 @@ const char* elf32_powerpc = "elf32-powerpc";
 const char* elf32_littlearm = "elf32-littlearm";
 const char* elf32_bigarm = "elf32-bigarm";
 char* get_bfd_target(const char* arch_name){
-	if(strcmp("powerpc", arch_name) == 0){
+	if(strncmp("powerpc", arch_name, sizeof("powerpc")) == 0){
 		return elf32_powerpc;
 	}
-	else if(strcmp("arm", arch_name) == 0 ){
+	else if(strncmp("arm", arch_name, sizeof("arm")) == 0 ){
 		return elf32_littlearm;
 	}
 	else
