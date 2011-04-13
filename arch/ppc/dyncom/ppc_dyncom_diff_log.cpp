af151e9449791be659bd6e8a3319758c4dda62b7
lizhijie <lizhijie86su@gmail.com>
2011-04-13 11:01:36 +0800

    change diff log for powerpc dyncom
    change diff log to just compare pc and modified register.The format
    of diff log file is like:
    
    10000140
    10000144  9 fffffff
    10000148  1 ffffff0
    1000014c
    10000150  1 fffffe0
    
    The first column is pc, and followed by modified register index
    and data.
    
    Signed-off-by: lizhijie <lizhijie86su@gmail.com>

--- arch/ppc/dyncom/ppc_dyncom_diff_log.cpp
+++ arch/ppc/dyncom/ppc_dyncom_diff_log.cpp
@@ -4,11 +4,32 @@
 static FILE *fp;
 static char* file_name = "pc_reg.log";
 static int flag = 0;
+static unsigned int last_gpr[32];
+#define LAST_PC_NUM 10
+static unsigned int pc_trace[LAST_PC_NUM] = {0};
+
+static void print_pc_trace(){
+	int i = LAST_PC_NUM - 1;
+	for(; i >= 0 ; i--)
+		printf("[pc trace : %d]\t0x%x\n", i, pc_trace[i]);
+}
+static int diff_log_over_flag = 0;
+static int is_diff_log_over(){
+	if(feof(fp)){
+		if(diff_log_over_flag == 0){
+			printf("##diff log over!\n");
+			diff_log_over_flag = 1;
+		}
+		return 1;
+	}else
+		return 0;
+}
 void ppc_dyncom_diff_log(const unsigned int pc,
 		const unsigned int lr,
 		const unsigned int cr,
 		const unsigned int ctr,
 		const unsigned int reg[]){
+	int i;
 	if(flag == 0){
 		if(!(fp = fopen(file_name, "r"))){
 			printf("failed open file %s\n", file_name);
@@ -16,45 +37,69 @@ void ppc_dyncom_diff_log(const unsigned int pc,
 		}
 		printf("ppc dyncom diff log!\n");
 		flag = 1;
+		for(i = 0; i < 32; i++)
+			last_gpr[i] = reg[i];
 	}
-	int i;
 	unsigned int pc_log = 0;
 	unsigned int lr_log = 0;
 	unsigned int cr_log = 0;
 	unsigned int ctr_log = 0;
-	unsigned int reg_log[32] = {0};
 	/* pc */
+	if(is_diff_log_over())
+		goto over;
 	fscanf(fp, "%x", &pc_log);
 	if(pc_log != pc){
 		fprintf(stderr, "\n\nIn %s:pc 0x%x error occured,should be 0x%x\n\n", __func__, pc, pc_log);
-		exit(0);
+		goto exit;
 	}
 	/* gpr */
 	for(i = 0; i < 32; i ++){
-		fscanf(fp, "%x", &reg_log[i]);
-		if(reg_log[i] != reg[i]){
-			fprintf(stderr, "pc=0x%x,NOTE:error pc is the last one! (pc-4) if no jump...\n", pc);
-			fprintf(stderr, "In %s:register gpr[%d]=0x%x error occured,should be 0x%x\n\n", __func__, i, reg[i], reg_log[i]);
-			exit(0);
+		if(is_diff_log_over())
+			goto over;
+		if(last_gpr[i] != reg[i]){
+			fscanf(fp, "%x", &last_gpr[i]);
+			fscanf(fp, "%x", &last_gpr[i]);
+			if(last_gpr[i] != reg[i]){
+				print_pc_trace();
+				fprintf(stderr, "pc=0x%x,NOTE:error pc is the last one! (pc-4) if no jump...\n", pc);
+				fprintf(stderr, "In %s:register gpr[%d]=0x%x error occured,should be 0x%x\n\n", __func__, i, reg[i], last_gpr[i]);
+				goto exit;
+			}
 		}
 	}
+#if 0
 	/* lr,cr,ctr */
+	if(is_diff_log_over())
+		goto over;
 	fscanf(fp, "%x", &lr_log);
 	fscanf(fp, "%x", &cr_log);
 	fscanf(fp, "%x", &ctr_log);
 	if(lr != lr_log){
+		print_pc_trace();
 		fprintf(stderr, "pc=0x%x,NOTE:error pc is the last one! (pc-4) if no jump...\n", pc);
 		fprintf(stderr, "In %s:lr error 0x%x should be 0x%x\n", __func__, lr, lr_log);
-		exit(0);
+		goto exit;
 	}
 	if(cr != cr_log){
+		print_pc_trace();
 		fprintf(stderr, "pc=0x%x,NOTE:error pc is the last one! (pc-4) if no jump...\n", pc);
 		fprintf(stderr, "In %s:cr error 0x%x should be 0x%x\n", __func__, cr, cr_log);
-		exit(0);
+		goto exit;
 	}
 	if(ctr != ctr_log){
+		print_pc_trace();
 		fprintf(stderr, "pc=0x%x,NOTE:error pc is the last one! (pc-4) if no jump...\n", pc);
 		fprintf(stderr, "In %s:ctr error 0x%x should be 0x%x\n", __func__, ctr, ctr_log);
-		exit(0);
+		goto exit;
 	}
+#endif
+	i = LAST_PC_NUM - 1;
+	for(; i > 0; i--)
+		pc_trace[i] = pc_trace[i - 1];
+	pc_trace[0] = pc;
+	return;
+exit:
+	exit(0);
+over:
+	return;
 }
