6ec7b4b1bd08c25f81b07159229c221d14a58138
Michael.Kang <blackfin.kang@gmail.com>
2011-04-25 14:20:06 +0800

    Add exec and cell object to perf monitor
    Add exec and cell object to perf monitor.Still can not work.
    
    Signed-off-by: Michael.Kang <blackfin.kang@gmail.com>

--- utils/perf_monitor/pmon.c
+++ utils/perf_monitor/pmon.c
@@ -7,6 +7,9 @@
 #include "sim_control.h"
 #include "skyeye_command.h"
 #include "skyeye_thread.h"
+#include "skyeye_exec.h"
+#include "skyeye_obj.h"
+#include "skyeye_cell.h"
 
 /* flag to enable performence monitor function. */
 static int enable_pmon_flag;
@@ -17,10 +20,11 @@ static const char* pmon_filename = "./pmon.log";
 /* fd of log_filename */
 static FILE* pmon_fd;
 
-static void pmon_count(generic_arch_t* arch_instance){
+static void pmon_count_start(conf_object_t* argp){
 	int seconds = 0;
 	uint32 steps = 0;
 	uint32 last_steps = 0;
+	generic_arch_t* arch_instance = (generic_arch_t*)(argp->obj);
 	/* Test if skyeye is in running state. */
 	while(!SIM_is_running())
 		;
@@ -30,6 +34,12 @@ static void pmon_count(generic_arch_t* arch_instance){
 		seconds++;
 		steps = arch_instance->get_step();
 		fprintf(pmon_fd,"In %d seconds, MIPS=%d\n", seconds, (steps - last_steps));
+	}
+}
+static void com_count_stop(char* arg){
+	enable_pmon_flag = 0;
+	if(pmon_fd != NULL){
+		fclose(pmon_fd);
 	}
 }
 
@@ -44,9 +54,16 @@ void com_pmon(char *arg) {
 	}
 	pthread_t id;
 	generic_arch_t* arch_instance = get_arch_instance("");
-	create_thread(pmon_count, arch_instance, &id);
+	create_thread(pmon_count_start, arch_instance, &id);
+	skyeye_exec_t* exec = create_exec();
+	exec->priv_data = get_conf_obj_by_cast(arch_instance, "generic_arch_t");
+	exec->run = pmon_count_start;
+	exec->stop = com_count_stop;
+	add_to_default_cell(exec);
 }
-
+static void com_pon_stop(char* arg){
+	enable_pmon_flag = 0;
+}
 /* some initialization for log functionality */
 int pmon_init(){
 	exception_t exp;
@@ -54,6 +71,7 @@ int pmon_init(){
 	//register_callback(log_pc_callback, Step_callback);
 	/* add correspinding command */
 	add_command("pmon", com_pmon, "enable the performance monitor.\n");
+	add_command("pmon-stop", com_count_stop, "disable the performance monitor.\n");
 }
 
 /* destruction function for log functionality */
