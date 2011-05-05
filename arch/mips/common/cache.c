6bb72ced83b524be07135251dbbf6463ad4423c1
Jeff Du <Jeffdo.du@gmail.com>
2011-05-05 14:17:13 +0800

    Add comment for mips cache.c
    Add comment for mips cache.c
    
    Signed-off-by: Jeff Du <Jeffdo.du@gmail.com>

--- arch/mips/common/cache.c
+++ arch/mips/common/cache.c
@@ -1,6 +1,39 @@
+/* Copyright (C)
+* 2011 - SkyeyeTeam
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*
+*/
+
+/**
+* @file cache.c
+* @brief decode cache instruction
+* @author skyeye team
+* @version
+* @date 2011-05-05
+*/
+
 #include "instr.h"
 #include "emul.h"
-
+/**
+* @brief decode the instructions about cache
+*
+* @param mstate mips state
+* @param instr  instr code
+*
+* @return Pipeline info
+*/
 int 
 decode_cache(MIPS_State* mstate, Instr instr) //Use in Cache Instruction, it's unuseable in R3000
 {
