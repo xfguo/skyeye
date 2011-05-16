702ed9ff13ef5eeadb47a81fa9cd5c33355410b3
Michael.Kang <blackfin.kang@gmail.com>
2011-05-16 15:43:02 +0800

    Add two condition for the start of BB
    Add two condition for the start of BB.
    
    Signed-off-by: Michael.Kang <blackfin.kang@gmail.com>

--- common/dyncom/basicblock.cpp
+++ common/dyncom/basicblock.cpp
@@ -36,6 +36,8 @@ is_start_of_basicblock(cpu_t *cpu, addr_t a)
 		 TAG_AFTER_COND |		/* instruction after a branch */
 		 TAG_AFTER_TRAP |		/* instruction after a trap */
 		 TAG_SYSCALL	|
+		 TAG_START_PAGE	|
+		 TAG_AFTER_EXCEPTION|	/* instruction after a exception instruction */
 		 TAG_AFTER_SYSCALL	|
 		 TAG_ENTRY))			/* client wants to enter guest code here */
 		&& (tag & TAG_CODE);	/* only if we actually tagged it */
