6e5f18038d0bdbd505747abc6cc807079fd07095
lizhijie <lizhijie86su@gmail.com>
2011-05-31 20:44:13 +0800

    Add count step support in IR
    Add a basic block in front of dispatch to count the steps remained
    staying in the JIT Function.If exceed TIME_OUT_THRESHOLD, force
    return.
    
    Signed-off-by: lizhijie <lizhijie86su@gmail.com>

--- common/dyncom/translate_all.cpp
+++ common/dyncom/translate_all.cpp
@@ -47,10 +47,20 @@ cpu_translate_all(cpu_t *cpu, BasicBlock *bb_ret, BasicBlock *bb_trap)
 	}
 	LOG("bbs: %d\n", bbs);
 
-	// create dispatch basicblock
+	/* create dispatch basicblock
+	 * create a bb to count the cycles stay inside the JIT Function.
+	 * If up to some threshold value, force to return.
+	 * */
 	BasicBlock* bb_dispatch = BasicBlock::Create(_CTX(), "dispatch", cpu->dyncom_engine->cur_func, 0);
-	Value *v_pc = new LoadInst(cpu->ptr_PHYS_PC, "", false, bb_dispatch);
-	SwitchInst* sw = SwitchInst::Create(v_pc, bb_ret, bbs, bb_dispatch);
+	BasicBlock* bb_real_dispatch = BasicBlock::Create(_CTX(), "real_dispatch", cpu->dyncom_engine->cur_func, 0);
+	LoadInst* v_icount = new LoadInst(cpu->ptr_ICOUNTER, "", false, bb_dispatch);
+	LoadInst* v_old_icount = new LoadInst(cpu->ptr_OLD_ICOUNTER, "", false, bb_dispatch);
+	Value *cycles =	BinaryOperator::Create(Instruction::Sub, v_icount, v_old_icount, "", bb_dispatch);
+	Value *gout = new ICmpInst(*bb_dispatch, ICmpInst::ICMP_UGT, cycles, CONST(TIMEOUT_THRESHOLD), "");
+	BranchInst::Create(bb_trap, bb_real_dispatch, gout, bb_dispatch);
+	// create dispatch basicblock
+	Value *v_pc = new LoadInst(cpu->ptr_PHYS_PC, "", false, bb_real_dispatch);
+	SwitchInst* sw = SwitchInst::Create(v_pc, bb_ret, bbs, bb_real_dispatch);
 
 	// translate basic blocks
 	bbaddr_map &bb_addr = cpu->dyncom_engine->func_bb[cpu->dyncom_engine->cur_func];
