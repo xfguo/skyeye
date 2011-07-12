8cf677a62d9e032c1da8bf9a143331aaa9c0430c
Michael.Kang <blackfin.kang@gmail.com>
2011-07-12 14:17:04 +0800

    Add debug macro
    Add debug macro

--- common/include/skyeye_log.h
+++ common/include/skyeye_log.h
@@ -1,8 +1,15 @@
 #ifndef __SKYEYE_LOG_H__
 #define __SKYEYE_LOG_H__
 #include "skyeye_types.h"
+#include <stdio.h>
 #ifdef __cplusplus
  extern "C" {
+#endif
+
+#ifdef DEBUG
+#define DBG(fmt, ...) do { fprintf(stderr, fmt, ## __VA_ARGS__); } while (0)
+#else
+#define DBG(fmt, ...) do { } while (0)
 #endif
 
 void skyeye_log(log_level_t log_level, const char* func_name, char* format,...);
