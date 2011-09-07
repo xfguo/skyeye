620b6128f600c559d7e7788b5bba0ea53f896f12
Michael.Kang <blackfin.kang@gmail.com>
2011-09-07 17:32:04 +0800

    Add exception handler into dyncom for kernel running
    Add exception handler into dyncom for kernel running
    
    Signed-off-by: Michael.Kang <blackfin.kang@gmail.com>

--- arch/arm/dyncom/arm_dyncom_interface.cpp
+++ arch/arm/dyncom/arm_dyncom_interface.cpp
@@ -30,23 +30,155 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <sys/time.h>
 #endif
 
+//#include "common/bus/io.h"
+#include "armemu.h"
+#include "arm_regformat.h"
 #include "armdefs.h"
 #include "armcpu.h"
 #include "skyeye_dyncom.h"
 #include "arm_dyncom_run.h"
 #include "arm_arch_interface.h"
 #include "arm_dyncom_translate.h"
+#include "arm_dyncom_mmu.h"
+#include "arm_dyncom_run.h"
 
 static void
 arm_reset_state ()
 {
 }
 
-static void per_cpu_step(arm_core_t * core){
+#if 1
+void
+arm_dyncom_abort(arm_core_t * state, ARMword vector)
+{
+    uint32_t eebit;
+	switch (vector) {
+	case ARMul_ResetV:	/* RESET */
+		break;
+	case ARMul_UndefinedInstrV:	/* Undefined Instruction */
+        state->Reg_undef[1] = state->Reg[15] + 4;
+        state->Spsr[UNDEFBANK] = state->Cpsr;
+        state->Cpsr = state->Cpsr & 0xfffffc40;
+        state->Cpsr |= 0x9b;
+        eebit = (state->CP15[CP15(CP15_CONTROL)] >> 25) & 1;
+        state->Cpsr |= (eebit << 9);
+
+        switch_mode(state, state->Cpsr & 0x1f);
+		break;
+	case ARMul_SWIV:	/* Software Interrupt */
+            state->Reg_svc[1] = state->Reg[15] + 4;
+            state->Spsr[SVCBANK] = state->Cpsr;
+            state->Cpsr = state->Cpsr & 0xfffffc40;
+            state->Cpsr |= 0x93;
+            eebit = (state->CP15[CP15(CP15_CONTROL)] >> 25) & 1;
+            state->Cpsr |= (eebit << 9);
+
+            switch_mode(state, state->Cpsr & 0x1f);
+		break;
+	case ARMul_PrefetchAbortV:	/* Prefetch Abort */
+            state->Reg_abort[1] = state->Reg[15] + 4;
+            state->Spsr[ABORTBANK] = state->Cpsr;
+            state->Cpsr = state->Cpsr & 0xfffffc40;
+            state->Cpsr |= 0x97;
+            eebit = (state->CP15[CP15(CP15_CONTROL)] >> 25) & 1;
+            state->Cpsr |= (eebit << 9);
+
+            switch_mode(state, state->Cpsr & 0x1f);
+            state->Aborted = 0;
+            state->abortSig = 0;
+
+		break;
+	case ARMul_DataAbortV:	/* Data Abort */
+            state->Reg_abort[1] = state->Reg[15] + 8;
+            state->Spsr[ABORTBANK] = state->Cpsr;
+            state->Cpsr = state->Cpsr & 0xfffffc40;
+            state->Cpsr |= 0x97;
+            eebit = (state->CP15[CP15(CP15_CONTROL)] >> 25) & 1;
+            state->Cpsr |= (eebit << 9);
+
+            switch_mode(state, state->Cpsr & 0x1f);
+            state->Aborted = 0;
+            state->abortSig = 0;
+		break;
+	case ARMul_AddrExceptnV:	/* Address Exception */
+            printf("AddrExceptnV\n");
+            exit(-1);
+		break;
+	case ARMul_IRQV:	/* IRQ */
+		//chy 2003-09-02 the if sentence seems no use
+    {
+                    state->Reg_irq[1] = state->Reg[15] + 4;
+                    printf("in %s R15 is %x\n", __FUNCTION__, state->Reg[15]);
+                    state->Spsr[IRQBANK] = state->Cpsr;
+                    state->Cpsr = state->Cpsr & 0xfffffc40;
+                    state->Cpsr |= 0x92;
+                    eebit = (state->CP15[CP15(CP15_CONTROL)] >> 25) & 1;
+                    state->Cpsr |= (eebit << 9);
+                    switch_mode(state, state->Cpsr & 0x1f);
+    }
+	break;
+	case ARMul_FIQV:	/* FIQ */
+		//chy 2003-09-02 the if sentence seems no use
+            printf("FIQV\n");
+            exit(-1);
+
+		break;
+	}
+
+	if (ARMul_MODE32BIT) {
+		if (state->CP15[CP15(CP15_CONTROL)] & CONTROL_VECTOR)
+			vector += 0xffff0000;	//for v4 high exception  address
+		if (state->vector_remap_flag)
+			vector += state->vector_remap_addr; /* support some remap function in LPC processor */
+		//ARMul_SetR15 (state, vector);
+        state->Reg[15] = vector;
+	}
+	else
+            ;
+}
+#endif
+static void per_cpu_step(conf_object_t * running_core){
+
+        arm_core_t *core = (arm_core_t *)running_core->obj;
+        machine_config_t* mach = get_current_mach();
+        ARM_CPU_State* cpu = get_current_cpu();
+        cpu_t *cpu_dyncom = (cpu_t*)get_cast_conf_obj(core->dyncom_cpu, "cpu_t");
 	arm_dyncom_run((cpu_t*)get_cast_conf_obj(core->dyncom_cpu, "cpu_t"));
+
+    if (core->Reg[15] == 0xc00101a0) {
+            /* undefine float-point instruction in kernel : fmrx */
+            arm_dyncom_abort(core, ARMul_UndefinedInstrV);
+    }
+    if (core->syscallSig) {
+            core->syscallSig = 0;
+            arm_dyncom_abort(core, ARMul_SWIV);
+    }
+    #if SYNC_WITH_INTERPRET
+    if (cpu_dyncom->icounter > 1951000 && is_int_in_interpret(cpu_dyncom))
+    {
+            while(core->NirqSig) {
+                    mach->mach_io_do_cycle(cpu);
+            }
+    }
+    #endif
+    if (core->abortSig) {
+            arm_dyncom_abort(core, core->Aborted);
+    }
+    if (!core->NirqSig) {
+             if (!(core->Cpsr & 0x80)) {
+                     #if SYNC_WITH_INTERPRET
+                     if (cpu_dyncom->icounter > 1951000 && !is_int_in_interpret(cpu_dyncom)) {
+                             return;
+                     }
+                     #endif
+                    arm_dyncom_abort(core, ARMul_IRQV);
+             }
+    }
+
+    mach->mach_io_do_cycle(cpu);
 }
 
-static void per_cpu_stop(arm_core_t * core){
+static void per_cpu_stop(conf_object_t * core){
 }
 
 static bool arm_cpu_init()
@@ -84,10 +216,13 @@ static bool arm_cpu_init()
 		arm_dyncom_init(core);
 
 		skyeye_exec_t* exec = create_exec();
-		//exec->priv_data = get_conf_obj_by_cast(core, "arm_core_t");
-		exec->priv_data = (conf_object_t*)core;
-		exec->run =  (void (*)(conf_object_t*))per_cpu_step;
-		exec->stop = (void (*)(conf_object_t*))per_cpu_stop;
+		exec->priv_data = get_conf_obj_by_cast(core, "arm_core_t");
+        //exec->priv_data = get_conf_obj_by_cast(core, "ARMul_State");
+		//exec->priv_data = (conf_object_t*)core;
+		//exec->run =  (void (*)(conf_object_t*))per_cpu_step;
+        exec->run =  per_cpu_step;
+		//exec->stop = (void (*)(conf_object_t*))per_cpu_stop;
+        exec->stop = per_cpu_stop;
 		add_to_default_cell(exec);
 	}
 
@@ -109,18 +244,6 @@ arm_init_state ()
 static void
 arm_step_once ()
 {
-	int i;
-	machine_config_t* mach = get_current_mach();
-	ARM_CPU_State* cpu = get_current_cpu();
-	/* workaround boot sequence for dual core, we need the first core initialize some variable for second core. */
-	arm_core_t* core;
-	for(i = 0; i < cpu->core_num; i++ ){
-		core = &cpu->core[i];
-		/* if CPU1_EN is set? */
-			per_cpu_step(core);
-	}
-	/* for peripheral */
-	mach->mach_io_do_cycle(cpu);
 }
 
 static void arm_stop(uint32_t id){
@@ -198,6 +321,22 @@ static exception_t arm_set_register_by_id(int id, uint32 value){
         return No_exp;
 }
 
+static exception_t arm_signal(interrupt_signal_t *signal){
+	ARMul_State *state = get_current_core();
+	arm_signal_t *arm_signal = &signal->arm_signal;
+	if (arm_signal->irq != Prev_level) {
+		state->NirqSig = arm_signal->irq;
+    }
+	if (arm_signal->firq != Prev_level)
+		state->NfiqSig = arm_signal->firq;
+
+	/* reset signal in arm dyf add when move sa1100 to soc dir  2010.9.21*/
+	if (arm_signal->reset != Prev_level)
+		state->NresetSig = arm_signal->reset;
+	return No_exp;
+}
+
+
 #ifdef __cplusplus
 	extern "C" {
 #endif
@@ -222,6 +361,7 @@ init_arm_dyncom ()
 	arm_arch->get_regname_by_id = arm_get_regname_by_id;
 	arm_arch->set_regval_by_id = arm_set_register_by_id;
 	arm_arch->get_regnum = arm_get_regnum;
+    arm_arch->signal = arm_signal;
 
 	register_arch (arm_arch);
 	return No_exp;
