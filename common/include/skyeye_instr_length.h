92a675e49b66575e5bfb69ccb6e6170254d43c72
Michael.Kang <blackfin.kang@gmail.com>
2011-11-14 17:20:33 +0800

    The byte length and instruction size definition
    The byte length and instruction size definition
    
    Signed-off-by: Michael.Kang <blackfin.kang@gmail.com>

---
+++ common/include/skyeye_instr_length.h
@@ -0,0 +1,34 @@
+/* Copyright (C) 
+* 2011 - Michael.Kang blackfin.kang@gmail.com
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+* 
+*/
+/**
+* @file skyeye_instr_length.h
+* @brief The instruction length definition
+* @author Michael.Kang blackfin.kang@gmail.com
+* @version 78.77
+* @date 2011-11-13
+*/
+
+#ifndef __SKYEYE_INSTR_LENGTH_H__
+#define __SKYEYE_INSTR_LENGTH_H__
+typedef enum{
+	Byte_1 = 1,
+	Byte_2,
+	Byte_3,
+	Byte_4
+}byte_length_t;
+#endif
